ef4e1069c243e11c5d89b2647cda4718b5bee119
rcavalcante11 <dicavalcante18@gmail.com>
2025-12-02 15:26:21 +0000

    feat: creating TabBar file and home VCa

--- AppBackFront/SceneDelegate.swift
+++ AppBackFront/SceneDelegate.swift
@@ -17,7 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let vc: LoginVC = LoginVC()
+//        let vc: LoginVC = LoginVC()
+        let vc: TabBarVC = TabBarVC()
         window.rootViewController = vc
         window.makeKeyAndVisible()
         self.window = window
--- TabBarController/Feature/Login/Screen/LoginScreen.swift
+++ TabBarController/Feature/Login/Screen/LoginScreen.swift
@@ -264,3 +264,4 @@ protocol LoginScreenProtocol: AnyObject {
     
     
 }
+ 
