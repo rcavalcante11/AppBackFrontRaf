f68daf3fe64e350c2df99fc773b8540b245cdb20
rcavalcante11 <dicavalcante18@gmail.com>
2026-01-22 15:07:28 +0000

    Homeervice and Json added

---
+++ AppBackFront/Service/HomeService/HomeService.swift
@@ -0,0 +1,18 @@
+//
+//  HomeService.swift
+//  AppBackFront
+//
+//  Created by macbook on 21/01/2026.
+//
+
+import UIKit
+
+protocol HomeServiceDelegate: GenericService {
+//    func getHomeFromJson (completion: @escaping completion <>)
+}
+
+class HomeService {
+    
+    
+
+}
